0eb70327bab42956c996d44c803f4effaec23059
SZT <1248300875@qq.com>
2016-04-02 16:00:28 +0800

    runtime下的成员变量操作

---
+++ FastTastRuntime/FastTastRuntime/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  FastTastRuntime
+//
+//  Created by SZT on 16/4/2.
+//  Copyright © 2016年 SZT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ FastTastRuntime/FastTastRuntime/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  FastTastRuntime
+//
+//  Created by SZT on 16/4/2.
+//  Copyright © 2016年 SZT. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
